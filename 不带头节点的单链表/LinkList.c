bae47ff32465c75452317c64e4c81288cfffe9b2
CC <59215677+1536068533@users.noreply.github.com>
2022-07-11 15:01:01 +0800

    Update LinkList.c

--- 不带头节点的单链表/LinkList.c
+++ 不带头节点的单链表/LinkList.c
@@ -1,7 +1,7 @@
 #include "LinkList.h"
 
 bool InitList(LinkList* pL) {
-	/*��ʼ��һ���յĵ�����������true*/
+	/*初始化一个空的单链表，返回true*/
 	*pL = (LinkList)malloc(sizeof(LNode));
 	if (*pL == NULL)
 		return false;
@@ -11,13 +11,13 @@ bool InitList(LinkList* pL) {
 }
 
 bool Empty(LinkList L) {
-	/*�жϵ������Ƿ�Ϊ������
-	�Ƿ��Ѿ���ʼ���������ڴ�ռ�*/
+	/*判断单链表是否为空链表
+	是否已经初始化开辟了内存空间*/
 	return (L == NULL);
 }
 
 LNode* GetElem(LinkList L, int i) {
-	/*����������ͷ��ַ������������������ض�Ӧ�����ڵ��ַ��û�иýڵ��򷵻�NULL*/
+	/*传入链表开头地址和链表坐标参数，返回对应链表节点地址，没有该节点则返回NULL*/
 	if (i < 1 || i > LinkListLen(L) || Empty(L))
 		return NULL;
 	else if (i == 1)
@@ -34,7 +34,7 @@ LNode* GetElem(LinkList L, int i) {
 }
 
 int LinkListLen(LinkList L) {
-	/*��ȡ��������*/
+	/*获取链表长度*/
 	if (Empty(L))
 		return 0;
 	else {
@@ -49,8 +49,8 @@ int LinkListLen(LinkList L) {
 }
 
 bool TailInsert(LinkList L, void* Ldata) {
-	/*β�巨��������β����������
-	���ӽڵ�ʧ���򷵻�false*/
+	/*尾插法，从链表尾部插入数据
+	添加节点失败则返回false*/
 	if (Empty(L))
 		return false;
 	else if (L->data == NULL && L->next == NULL) {
@@ -59,7 +59,7 @@ bool TailInsert(LinkList L, void* Ldata) {
 	}
 	else {
 		LNode* s = (LinkList)malloc(sizeof(LNode));
-		if (s == NULL)  //�����ڴ���ܻ����ʧ�ܵ���������ڴ�������������false
+		if (s == NULL)  //分配内存可能会出现失败的情况（如内存已满），返回false
 			return false;
 		GetElem(L, LinkListLen(L))->next = s;
 		s->data = Ldata;
@@ -69,7 +69,7 @@ bool TailInsert(LinkList L, void* Ldata) {
 }
 
 bool HeadInsert(LinkList L, void* Ldata) {
-	/*ͷ�巨��������ͷ����������*/
+	/*头插法，从链表头部插入数据*/
 	if (Empty(L))
 		return false;
 	else if (L->data == NULL && L->next == NULL) {
@@ -78,7 +78,7 @@ bool HeadInsert(LinkList L, void* Ldata) {
 	}
 	else {
 		LNode* s = (LNode*)malloc(sizeof(LNode));
-		if (s == NULL)  //�ڴ����ʧ��
+		if (s == NULL)  //内存分配失败
 			return false;
 		s->data = L->data;
 		s->next = L->next;
@@ -89,7 +89,7 @@ bool HeadInsert(LinkList L, void* Ldata) {
 }
 
 bool IndexInsert(LinkList L, int i, void* Ldata) {
-	/*��λ����ڵ㣬���������±����ڵ�*/
+	/*按位插入节点，即按链表下标插入节点*/
 	if (i<1 || i>LinkListLen(L) + 1 || Empty(L))
 		return false;
 	else if (L->data == NULL && L->next == NULL && i == 2) {
@@ -97,12 +97,12 @@ bool IndexInsert(LinkList L, int i, void* Ldata) {
 	}
 	if (i == 1)
 		return HeadInsert(L, Ldata);
-	while (i > 2) {  //����ѭ���ҵ�Ҫ����������ڵ��±��ǰһ�������ڵ�
+	while (i > 2) {  //利用循环找到要插入的链表节点下标的前一个链表节点
 		L = L->next;
 		i--;
 	}
 	LNode* s = (LNode*)malloc(sizeof(LNode));
-	if (s == NULL)  //�ڴ����ʧ��
+	if (s == NULL)  //内存分配失败
 		return false;
 	s->data = Ldata;
 	s->next = L->next;
@@ -111,10 +111,10 @@ bool IndexInsert(LinkList L, int i, void* Ldata) {
 }
 
 bool TailDelete(LinkList L) {
-	/*ɾ������ĩβ�ڵ�
-	�����������������Ǹ�����������ʱû��ɾ���κνڵ㣩�򷵻�false
-	�������ֻ��һ���ڵ㣬��ɾ���ýڵ�����ݣ��������ͷ��ڴ棬��󷵻�true
-	������������������ڵ㣬���ͷ�ĩβ�ڵ��ڴ沢����true*/
+	/*删除链表末尾节点
+	如果传入的链表本身是个空链表（此时没有删除任何节点）则返回false
+	如果链表只有一个节点，则删除该节点的数据，但不会释放内存，最后返回true
+	如果链表有至少两个节点，则释放末尾节点内存并返回true*/
 	if (L == NULL)
 		return false;
 	else if (L->next == NULL) {
@@ -128,10 +128,10 @@ bool TailDelete(LinkList L) {
 }
 
 bool IndexDelete(LinkList L, int i) {
-	/*���������±�ɾ���ڵ�
-	�����������������Ǹ�����������ʱû��ɾ���κνڵ㣩����������±겻�ڷ�Χ���򷵻�false
-	���ָ��ɾ������Ψһ�Ľڵ㣬��ɾ���ýڵ����ݣ��������ͷ��ڴ棬��󷵻�true
-	������������������ڵ㣬���ͷ�ָ���ڵ��ڴ沢����true*/
+	/*根据链表下标删除节点
+	如果传入的链表本身是个空链表（此时没有删除任何节点）或传入的链表下标不在范围内则返回false
+	如果指定删除链表唯一的节点，则删除该节点数据，但不会释放内存，最后返回true
+	如果链表有至少两个节点，则释放指定节点内存并返回true*/
 	int len = LinkListLen(L);
 	if (i<=0 || i > len)
 		return false;
@@ -148,9 +148,9 @@ bool IndexDelete(LinkList L, int i) {
 }
 
 bool DeleteNode(LinkList L, LNode* p) {
-	/*���ݴ���������ڵ��ַɾ�������ڵ�
-	�����������������Ǹ�����������ʱû��ɾ���κνڵ㣩�򷵻�false
-	������������������ڵ㣬���ͷ�ָ���ڵ��ڴ沢����true*/
+	/*根据传入的链表节点地址删除链表节点
+	如果传入的链表本身是个空链表（此时没有删除任何节点）则返回false
+	如果链表有至少两个节点，则释放指定节点内存并返回true*/
 	if (p == NULL)
 		return false;
 	else if (p->next == NULL)
@@ -171,7 +171,7 @@ bool DeleteNode(LinkList L, LNode* p) {
 }
 
 LNode* LocateELem(LinkList L, void* Ldata) {
-	/*��ֵ����*/
+	/*按值查找*/
 	if (L == NULL)
 		return NULL;
 	else if (L->data == Ldata)
@@ -185,10 +185,10 @@ LNode* LocateELem(LinkList L, void* Ldata) {
 }
 
 bool HeadDelete(LinkList L) {
-	/*ɾ��ͷ�ڵ�
-	�����������������Ǹ�����������ʱû��ɾ���κνڵ㣩�򷵻�false
-	�������ֻ��һ���ڵ���ɾ���ýڵ�����ݣ��������ͷ��ڴ棬��󷵻�true
-	������������������ڵ㣬���ͷŵ�һ���ڵ��ڴ沢����true*/
+	/*删除头节点
+	如果传入的链表本身是个空链表（此时没有删除任何节点）则返回false
+	如果链表只有一个节点则删除该节点的数据，但不会释放内存，最后返回true
+	如果链表有至少两个节点，则释放第一个节点内存并返回true*/
 	if (L == NULL)
 		return false;
 	else if (L->next == NULL) {
@@ -205,17 +205,12 @@ bool HeadDelete(LinkList L) {
 }
 
 bool DeleteLinkList(LinkList* pL) {
-	/*ɾ���������нڵ㲢�ͷſռ䣬�������ص���ʼ��֮ǰ��״̬
-	�����������������Ǹ�����������ʱû��ɾ���κνڵ㣩�򷵻�false
-	�����ɾ���ڵ�����򷵻�true*/
+	/*删除链表所有节点并释放空间，让链表回到初始化之前的状态
+	如果传入的链表本身是个空链表（此时没有删除任何节点）则返回false
+	如果有删除节点操作则返回true*/
 	if (*pL == NULL)
 		return false;
 	else {
-		int len = LinkListLen(*pL);
-		while (len > 1) {
-			HeadDelete(*pL);
-			len--;
-		}
 		free(*pL);
 		*pL = NULL;
 		return true;
