ac1c9b9255ad597fccc5f80e60e5592efbe7fe07
CC <59215677+1536068533@users.noreply.github.com>
2022-09-27 14:02:38 +0800

    Update TextTalkServer.scala

--- Akka/src/main/scala/Chat/server/TextTalkServer.scala
+++ Akka/src/main/scala/Chat/server/TextTalkServer.scala
@@ -9,24 +9,27 @@ import scala.util.control.Breaks.{break, breakable}
 class TextTalkServer extends Actor {
   override def receive: Receive = {
     case "start" => println("start 聊天服务器开始工作了") //可以用于初始化，做一些准备工作
-    case userData: UserInformation => TextTalkServer.userInformation(userData.user) = userData.userActorRef
-    case _: UserList => sender() ! UserList(TextTalkServer.userInformation) //如果有人查询用户列表，则返回用户列表
-    case message: MessageProtocol =>
-      println("\n" + message.user + "发来消息: " + message.message)
-      print("Server@" + TextTalkServer.host + ":" + TextTalkServer.port + "# ")
+    case userData: RegisterUser =>
+      //用户名已存在，向用户答复注册失败，否则，加入用户信息表并向用户答复注册成功
+      if (TextTalkServer.userInformation.contains(userData.userName)) sender() ! RegisteredUser(false)
+      else{
+        TextTalkServer.userInformation(userData.userName) = userData.userActorRef
+        sender() ! RegisteredUser(true)
+      }
+    case UserList => sender() ! UserList(TextTalkServer.userInformation) //如果有人查询用户列表，则返回用户列表
+    case message: MessageProtocol => println(message.user + "发来消息: " + message.message)
     case user: Offline => TextTalkServer.userInformation -= user.user
     case "shutdown" =>
       println("接收到shutdown指令，退出系统")
-      context.stop(self)  //这条语句和下面的语句用来退出akka
+      context.stop(self) //这条语句和下面的语句用来退出akka
       context.system.terminate()
   }
 }
 
 object TextTalkServer {
   val userInformation = new scala.collection.mutable.HashMap[String, ActorRef] //用户名和对应的ActorRef的映射表
-  val host = "127.0.0.1"  //指定本服务器的ip
-  val port = 9999 //指定本服务器的端口
   def main(args: Array[String]): Unit = {
+    val (host, port) = (args(0), 9999) //指定本服务器的ip和端口
     //用于配置本服务器的ip和端口，”akka.actor.allow-java-serialization = "on"“表示使用java来序列化，远程发送消息需要经过序列化
     val config = ConfigFactory.parseString(
       s"""
@@ -39,21 +42,19 @@ object TextTalkServer {
     val serverActorSystem = ActorSystem("Server", config) //“Server”指定的是ActorSystem的名字
     //“TextTalkServer”指定的是Actor的名字
     val chatServerActorRef: ActorRef = serverActorSystem.actorOf(Props[TextTalkServer], "TextTalkServer")
-    userInformation("server") = chatServerActorRef  //将自身的ActorRef先加入到userInformation表中，可以让其它用户向服务器发消息
-    chatServerActorRef ! "start"  //给自己发送消息，是初始化的一种方法
+    userInformation("server") = chatServerActorRef //将自身的ActorRef先加入到userInformation表中，可以让其它用户向服务器发消息
+    chatServerActorRef ! "start" //给自己发送消息，是初始化的一种方法
     breakable(
       while (true) {
-        Thread.sleep(500) //延迟只是为了输出的格式好看，否则下面的Server@……#会和akka的信息不换行而叠在一起
         println("-" * 100)
         println("ls->查看用户表\tshutdown->关闭服务器")
-        print("Server@" + host + ":" + port + "# ") //模仿Linux命令行界面
-        val order = StdIn.readLine()  //接收输入
+        val order = StdIn.readLine() //接收输入
         order match { //根据输入的字符串判断指令做对应操作
-          case "ls" => for ((k, v) <- userInformation) println(k + "\t" + v)  //展示用户信息表
-          case "shutdown" =>  //关闭服务器
+          case "ls" => for ((k, v) <- userInformation) println(k + "\t" + v) //展示用户信息表
+          case "shutdown" => //关闭服务器
             chatServerActorRef ! "shutdown"
             break()
-          case user: String =>  //给用户发送消息
+          case user: String => //给用户发送消息
             if (userInformation.contains(user)) {
               print("发送消息@" + user + ": ")
               val mes = StdIn.readLine()
